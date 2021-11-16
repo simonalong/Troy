9ccd9666e08580814334770595bb655fc237440f
shizi <lan.zhu.123@163.com>
2021-11-16 18:37:38 +0800

    update 更新help

--- troy/src/main/java/com/github/simonalong/troy/endpoint/HelpEndpoint.java
+++ troy/src/main/java/com/github/simonalong/troy/endpoint/HelpEndpoint.java
@@ -4,6 +4,9 @@ import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static com.github.simonalong.troy.TroyConstants.HELP;
 
 /**
@@ -21,37 +24,46 @@ public class HelpEndpoint {
      *
      * @return 分组名列表
      */
-    @SuppressWarnings("all")
     @ReadOperation
-    public String getList() {
-        StringBuffer apiMessage = new StringBuffer();
-        apiMessage.append("===========================================================================================================").append("\n");
-        apiMessage.append("help 查询：命令列表         ").append("curl " + apiPrefix() + "/help").append("\n");
-        apiMessage.append("help 查询：修改ip和port     ").append("curl " + withJson() + " '" + apiPrefix() + "/help?ip={ip}&port={port}'").append("\n");
-        apiMessage.append("=========== 分组：========================================").append("\n");
-        apiMessage.append("查询：分组列表              ").append("curl '" + apiPrefix() + "/group/list'").append("\n");
-        apiMessage.append("查询：分组函数列表          ").append("curl '" + apiPrefix() + "/group/fun/list?group={group}'").append("\n");
-        apiMessage.append("变更：更新分组信息          ").append("curl " + withJson() + " '" + apiPrefix() + "/group?group={group}&printLogLevel={logLevel}&enable={enable}'").append("\n");
-        apiMessage.append("变更：某个函数更新          ").append("curl " + withJson() + " '" + apiPrefix() + "/group/fun/change?funId={funId}&printLogLevel={logLevel}&enable={enable}'").append("\n");
-        apiMessage.append("------日志------------").append("\n");
-        apiMessage.append("日志查询：分组某个函数信息  ").append("curl '" + apiPrefix() + "/group/fun/info/one/logger?logFunId={funId}'").append("\n");
-        apiMessage.append("日志查询：分组函数全部信息  ").append("curl '" + apiPrefix() + "/group/fun/info/all?group={group}'").append("\n");
-        apiMessage.append("------输出------------").append("\n");
-        apiMessage.append("输出变更：添加分组输出      ").append("curl " + withJson() + " '" + apiPrefix() + "/group/(console/file/all)?group={group}&printLogLevel={logLevel}&enable={enable}'").append("\n");
-        apiMessage.append("输出变更：添加函数输出      ").append("curl " + withJson() + " '" + apiPrefix() + "/group/fun/print/(console/file/all)?funId={funId}&printLogLevel={logLevel}&enable={enable}'").append("\n");
-        apiMessage.append("=========== logger：======================================").append("\n");
-        apiMessage.append("查询：logger列表            ").append("curl '" + apiPrefix() + "/logger'").append("\n");
-        apiMessage.append("查询：logger搜索            ").append("curl '" + apiPrefix() + "/logger/{loggerName}'").append("\n");
-        apiMessage.append("更新：logger更新级别        ").append("curl " + withJson() + " '" + apiPrefix() + "/logger?loggerName={loggerName}&logLevel={logLevel}'").append("\n");
-        apiMessage.append("更新：logger更新级别        ").append("curl " + withJson() + " '" + apiPrefix() + "/logger/appender/(console/file/all)?loggerName={loggerName}&logLevel={logLevel}'").append("\n");
-        apiMessage.append("更新：logger处理恢复        ").append("curl " + withJson() + " '" + apiPrefix() + "/logger/restore/all/info?loggerName={loggerName}&logLevel={logLevel}'").append("\n");
-        apiMessage.append("=========== appender：====================================").append("\n");
-        apiMessage.append("更新：添加输出器            ").append("curl " + withJson() + " '" + apiPrefix() + "/appender/(console/file/all)?loggerName={loggerName}&logLevel={logLevel}'").append("\n");
-        apiMessage.append("删除：删除某个输出器        ").append("curl -X DELETE '" + apiPrefix() + "/appender?loggerName={loggerName}'").append("\n");
-        apiMessage.append("删除：删除输出器            ").append("curl -X DELETE '" + apiPrefix() + "/appender/(console/file/all)?loggerName={loggerName}'").append("\n");
-        apiMessage.append("============================================================================================================").append("\n");
+    public Object getList() {
+        Map<String, Object> infoMap = new LinkedHashMap<>();
 
-        return apiMessage.toString();
+        Map<String, String> helpMap = new LinkedHashMap<>();
+        helpMap.put("help 查询：命令列表", "curl " + apiPrefix() + "/help");
+        helpMap.put("help 查询：修改ip和port", "curl " + withJson() + " '" + apiPrefix() + "/help?ip={ip}&port={port}'");
+        infoMap.put("help", helpMap);
+
+        Map<String, String> groupMap = new LinkedHashMap<>();
+        groupMap.put("查询：分组列表", "curl '" + apiPrefix() + "/group/list'");
+        groupMap.put("查询：分组函数列表", "curl '" + apiPrefix() + "/group/fun/list?group={group}'");
+        groupMap.put("变更：更新分组信息", "curl " + withJson() + " '" + apiPrefix() + "/group?group={group}&printLogLevel={logLevel}&enable={enable}'");
+        groupMap.put("变更：某个函数更新", "curl " + withJson() + " '" + apiPrefix() + "/group/fun/change?funId={funId}&printLogLevel={logLevel}&enable={enable}'");
+        infoMap.put("分组", groupMap);
+
+        Map<String, String> groupLogMap = new LinkedHashMap<>();
+        groupLogMap.put("日志查询：分组某个函数信息", "curl '" + apiPrefix() + "/group/fun/info/one/logger?logFunId={funId}'");
+        groupLogMap.put("日志查询：分组函数全部信息", "curl '" + apiPrefix() + "/group/fun/info/all?group={group}'");
+        infoMap.put("分组-日志", groupLogMap);
+
+        Map<String, String> groupPrintMap = new LinkedHashMap<>();
+        groupPrintMap.put("输出变更：添加分组输出", "curl " + withJson() + " '" + apiPrefix() + "/group/(console/file/all)?group={group}&printLogLevel={logLevel}&enable={enable}'");
+        groupPrintMap.put("输出变更：添加函数输出", "curl " + withJson() + " '" + apiPrefix() + "/group/fun/print/(console/file/all)?funId={funId}&printLogLevel={logLevel}&enable={enable}'");
+        infoMap.put("分组-输出", groupPrintMap);
+
+        Map<String, String> loggerMap = new LinkedHashMap<>();
+        loggerMap.put("查询：logger列表", "curl '" + apiPrefix() + "/logger'");
+        loggerMap.put("查询：logger搜索", "curl '" + apiPrefix() + "/logger/{loggerName}'");
+        loggerMap.put("更新：logger更新并输出", "curl " + withJson() + " '" + apiPrefix() + "/logger/appender/(console/file/all)?loggerName={loggerName}&logLevel={logLevel}'");
+        loggerMap.put("更新：logger处理恢复", "curl " + withJson() + " '" + apiPrefix() + "/logger/restore/all/info?loggerName={loggerName}&logLevel={logLevel}'");
+        infoMap.put("logger", loggerMap);
+
+        Map<String, String> appenderMap = new LinkedHashMap<>();
+        appenderMap.put("更新：添加输出器", "curl " + withJson() + " '" + apiPrefix() + "/appender/(console/file/all)?loggerName={loggerName}&logLevel={logLevel}'");
+        appenderMap.put("删除：删除某个输出器", "curl -X DELETE '" + apiPrefix() + "/appender?loggerName={loggerName}'");
+        appenderMap.put("删除：删除输出器", "curl -X DELETE '" + apiPrefix() + "/appender/(console/file/all)?loggerName={loggerName}'");
+        infoMap.put("appender", appenderMap);
+
+        return infoMap;
     }
 
     /**
@@ -72,12 +84,12 @@ public class HelpEndpoint {
 
     private String apiPrefix() {
         String apiPre = "/api/troy/log";
-        if(null != System.getProperty("management.endpoints.web.basePath") && !"".equals(System.getProperty("management.endpoints.web.basePath"))) {
+        if (null != System.getProperty("management.endpoints.web.basePath") && !"".equals(System.getProperty("management.endpoints.web.basePath"))) {
             apiPre = System.getProperty("management.endpoints.web.basePath");
         }
 
         if (null != apiPre && apiPre.endsWith("/")) {
-            apiPre = apiPre.substring(0, apiPre.length() -1);
+            apiPre = apiPre.substring(0, apiPre.length() - 1);
         }
 
         if (null != localIp && !"".endsWith(localIp) && 0 != port) {
